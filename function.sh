3f108385964d0a9b95cea94e814d85fc36c0da78
Hari krishna <kothakoti.harikrishna@gmail.com>
2023-08-30 00:13:55 +0200

    function example

---
+++ function.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo " $2......is failure"
+        exit 1
+    else 
+        echo " $2.......is success"
+        exit 1
+    fi    
+}
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then
+    echo "please switch to root user to install"
+    exit 1
+fi
+
+yum install git -y
+validate $? "installing of git"
